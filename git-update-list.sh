89611165f88e00fd1bdf361ce185c201ee666c4d
Shumen Won <hsm@localhost.localdomain>
2013-09-14 10:45:05 +0800

    track all branches

--- git-update-list.sh
+++ git-update-list.sh
@@ -1,17 +1,19 @@
-( cd linux-vd_agent && git pull --all && git push -u github --mirror );
-( cd qemu && git pull --all && git push -u github --mirror );
-( cd slirp && git pull --all && git push -u github --mirror );
-( cd spice && git pull --all && git push -u github --mirror );
-( cd spice-common && git pull --all && git push -u github --mirror );
-( cd spice-gtk && git pull --all && git push -u github --mirror );
-( cd spice-html5 && git pull --all && git push -u github --mirror );
-( cd spice-protocol && git pull --all && git push -u github --mirror );
-( cd spice-xpi && git pull --all && git push -u github --mirror );
-( cd spicec && git pull --all && git push -u github --mirror );
-( cd usbredir && git pull --all && git push -u github --mirror );
-( cd usbclerk && git pull --all && git push -u github --mirror );
-( cd vdesktop && git pull --all && git push -u github --mirror );
-( cd qxl && git pull --all && git push -u github --mirror );
-( cd win32-vd_agent && git pull --all && git push -u github --mirror );
-( cd vdi_port && git pull --all && git push -u github --mirror );
-( cd virt-viewer && git pull --all && git push -u github --mirror );
+# 'git pull --all' assumes all branches are tracked, if they aren't, use git branch --track $remote
+# for branch in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track ${branch##*/} $branch; done
+( cd linux-vd_agent && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd qemu && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd slirp && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice-common && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice-gtk && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice-html5 && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice-protocol && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spice-xpi && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd spicec && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd usbredir && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd usbclerk && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd vdesktop && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd qxl && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd win32-vd_agent && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd vdi_port && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
+( cd virt-viewer && for remote in `git branch -r | grep -v github | grep -v HEAD | grep -v master`; do git branch --track $remote; done; git pull --all && git push -u github --mirror );
